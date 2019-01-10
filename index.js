78739aed33b829aaa45da3e8ab40f48c2555a33a
Ivan Kalinin <ikalinin@condenast.ru>
2019-01-10 11:56:08 +0300

    Add handling of if-modified-since header

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var s3 = new (require('aws-sdk')).S3({params:{Bucket: process.env.S3_BUCKET_NAME}});
+var s3 = new (require('aws-sdk')).S3({ params: { Bucket: process.env.S3_BUCKET_NAME } });
 
 module.exports = {
 
-	requestReceived: function(req, res, next) {
-		if(req.method !== 'GET') {
+	requestReceived: function (req, res, next) {
+		if (!['HEAD', 'GET'].includes(req.method)) {
 			return next();
 		}
 
@@ -14,10 +14,26 @@ module.exports = {
 		}
 
 		s3.getObject({
-				Key: key
+			Key: key
 		}, function (err, result) {
 
 			if (!err && result) {
+				var ifModifiedSince = new Date(req.headers['if-modified-since']);
+				var lastModified = new Date(result['LastModified']);
+				var now = Date.now();
+
+				if ('LastModified' in result) {
+					res.setHeader('Last-Modified', lastModified.toUTCString());
+				}
+
+				if (ifModifiedSince &&
+					lastModified &&
+					ifModifiedSince < now &&
+					lastModified < ifModifiedSince) {
+
+					return res.send(304);
+				}
+
 				return res.send(200, result.Body);
 			}
 
@@ -25,13 +41,13 @@ module.exports = {
 		});
 	},
 
-	pageLoaded: function(req, res, next) {
-		if(req.prerender.statusCode !== 200) {
+	pageLoaded: function (req, res, next) {
+		if (req.prerender.statusCode !== 200) {
 			return next();
 		}
 
 		var key = req.prerender.url;
-		
+
 		if (process.env.S3_PREFIX_KEY) {
 			key = process.env.S3_PREFIX_KEY + '/' + key;
 		}
@@ -41,7 +57,7 @@ module.exports = {
 			ContentType: 'text/html;charset=UTF-8',
 			StorageClass: 'REDUCED_REDUNDANCY',
 			Body: req.prerender.content
-		}, function(err, result) {
+		}, function (err, result) {
 
 			if (err) console.error(err);
 
