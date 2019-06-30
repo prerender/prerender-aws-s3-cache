11accd9989cc1a5eca2d9d625d445a4406089830
Marlon Marcos <marlon.marcos21@gmail.com>
2019-07-01 00:43:07 +0800

    Allow setting a cache freshness

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
-var s3 = new (require('aws-sdk')).S3({params:{Bucket: process.env.S3_BUCKET_NAME}});
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({params: {Bucket: process.env.S3_BUCKET_NAME}});
 
 module.exports = {
+  requestReceived: function(req, res, next) {
+    if (req.method !== 'GET') {
+      return next();
+    }
 
-	requestReceived: function(req, res, next) {
-		if(req.method !== 'GET') {
-			return next();
-		}
+    s3.getObject({Key: this.getCacheKey(req)}, (err, result) => {
+      if (!err && result) {
+        const freshness = process.env.S3_CACHE_FRESHNESS || 604800000;
+        const now = Date.now();
+        const lastModified = result.LastModified.getTime();
+        if ((now - lastModified) < freshness) {
+          return res.send(200, result.Body);
+        }
+      }
 
-		var key = req.prerender.url;
+      next();
+    });
+  },
 
-		if (process.env.S3_PREFIX_KEY) {
-			key = process.env.S3_PREFIX_KEY + '/' + key;
-		}
+  pageLoaded: function(req, res, next) {
+    if(req.prerender.statusCode !== 200) {
+      return next();
+    }
 
-		s3.getObject({
-				Key: key
-		}, function (err, result) {
+    const s3Options = {
+      Key: this.getCacheKey(req),
+      ContentType: 'text/html;charset=UTF-8',
+      StorageClass: 'REDUCED_REDUNDANCY',
+      Body: req.prerender.content
+    }
 
-			if (!err && result) {
-				return res.send(200, result.Body);
-			}
+    s3.putObject(s3Options, (err, result) => {
+      if (err) console.error(err);
+      next();
+    });
+  },
 
-			next();
-		});
-	},
-
-	pageLoaded: function(req, res, next) {
-		if(req.prerender.statusCode !== 200) {
-			return next();
-		}
-
-		var key = req.prerender.url;
-		
-		if (process.env.S3_PREFIX_KEY) {
-			key = process.env.S3_PREFIX_KEY + '/' + key;
-		}
-
-		s3.putObject({
-			Key: key,
-			ContentType: 'text/html;charset=UTF-8',
-			StorageClass: 'REDUCED_REDUNDANCY',
-			Body: req.prerender.content
-		}, function(err, result) {
-
-			if (err) console.error(err);
-
-			next();
-		});
-	}
+  getCacheKey: function(req) {
+    let key = req.prerender.url;
+    if (req.prerender.width) {
+      key = `${key}-width-${req.prerender.width}`;
+    }
+    if (process.env.S3_PREFIX_KEY) {
+      key = `${process.env.S3_PREFIX_KEY}/${key}`;
+    }
+    return key;
+  },
 };
